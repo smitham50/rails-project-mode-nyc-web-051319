f53ee626c73c752404dadd5981c0a946e053fce6
Aaron Smith <smitham50@gmail.com>
2019-06-18 07:07:21 -0400

    added distance helper method to index to clean up code

--- Trail-app/app/models/trail.rb
+++ Trail-app/app/models/trail.rb
@@ -5,7 +5,7 @@ class Trail < ApplicationRecord
   include RestClient
 
   def self.get_trail_info
-    trail_json = RestClient.get('https://www.hikingproject.com/data/get-trails?lat=40.71427&lon=-74.00597&maxDistance=150&maxResults=250&key=200492212-d7400571b0620563169df18724f8dc46')
+    trail_json = RestClient.get('https://www.hikingproject.com/data/get-trails?lat=40.71427&lon=-74.00597&maxDistance=100&maxResults=500&key=200492212-d7400571b0620563169df18724f8dc46')
     @trail_hash = JSON.parse(trail_json)
     @trail_hash
   end
