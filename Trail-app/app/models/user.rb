724f70ab36706f03878acae9eccefdd8f948947c
Aaron Smith <smitham50@gmail.com>
2019-06-19 13:28:19 -0400

    took out user address features

--- Trail-app/app/models/user.rb
+++ Trail-app/app/models/user.rb
@@ -3,9 +3,14 @@ class User < ApplicationRecord
   has_many :user_trails
   has_many :trails, through: :user_trails
   has_many :reviews
+
 # validates :username, presence: true
 # validates :username, uniquness: true
 # validates :password, presence: true
 # validates :password, confirmation: { case_sensitive: true }
+
+  def user_address
+    "#{self.address}, #{self.city}, #{self.state} #{self.zip_code}"
+  end
 
 end
