585f91eb4e82e6b96fe9f256872ad8c949ef709d
Aaron Smith <smitham50@gmail.com>
2019-06-21 13:34:31 -0400

    changed logout route to landing

--- Trail-app/app/controllers/sessions_controller.rb
+++ Trail-app/app/controllers/sessions_controller.rb
@@ -18,7 +18,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to login_path
+    redirect_to landing_path
   end
 
 end
--- Trail-app/config/routes.rb
+++ Trail-app/config/routes.rb
@@ -12,7 +12,7 @@ resources :reviews, only: [:new, :create, :destroy]
 get '/signup', to: "users#new", as: "signup"
 get '/show/:id', to: "users#show", as: "user"
 
-get '/', to: "users#landing"
+get '/', to: "users#landing", as: "landing"
 get '/login', to: "sessions#new", as: "login"
 post '/login', to: "sessions#create"
 delete "/logout", to: "sessions#destroy"
