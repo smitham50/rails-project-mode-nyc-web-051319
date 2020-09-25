df23645a6db26062d8112cf74da9f5c6f11620ec
Aaron Smith <smitham50@gmail.com>
2020-09-25 13:20:59 -0400

    cleaned up seed_trails method

--- Trail-app/db/seeds.rb
+++ Trail-app/db/seeds.rb
@@ -5,7 +5,29 @@ def seed_trails
     if key == "trails"
     trail_array.each do |trail|
       puts "Creating #{trail["name"]}"
-      Trail.create!(name: trail["name"], trail_type: trail["type"], summary: trail["summary"], difficulty: trail["difficulty"], stars: trail["stars"], starVotes: trail["starVotes"], location: trail["location"], url: trail["url"], imgSqSmall: trail["imgSqSmall"], imgSmall: trail["imgSmall"], imgSmallMed: trail["imgSmallMed"], imgMedium: trail["imgMedium"], length: trail["length"], ascent: trail["ascent"], descent: trail["descent"], high: trail["high"], low: trail["low"], longitude: trail["longitude"], latitude: trail["latitude"], conditionStatus: trail["conditionStatus"], conditionDetails: trail["conditionDetails"], conditionDate: trail["conditionDate"])
+      Trail.create!(
+        name: trail["name"], 
+        trail_type: trail["type"], 
+        summary: trail["summary"], 
+        difficulty: trail["difficulty"], 
+        stars: trail["stars"], 
+        starVotes: trail["starVotes"], 
+        location: trail["location"], 
+        url: trail["url"], 
+        imgSqSmall: trail["imgSqSmall"], 
+        imgSmall: trail["imgSmall"], 
+        imgSmallMed: trail["imgSmallMed"], 
+        imgMedium: trail["imgMedium"], 
+        length: trail["length"], 
+        ascent: trail["ascent"], 
+        descent: trail["descent"], 
+        high: trail["high"], 
+        low: trail["low"], 
+        longitude: trail["longitude"], 
+        latitude: trail["latitude"], 
+        conditionStatus: trail["conditionStatus"], 
+        conditionDetails: trail["conditionDetails"], 
+        conditionDate: trail["conditionDate"])
       end
     end
   end
